87c85690b58b38490f6cc4dac7dd54caf619edad
weiLaoShi <weilingwei_it@163.com>
2019-12-25 10:32:58 +0800

    youlaiyouqu-boss-整合工具

---
+++ src/main/java/com/youlaiyouqu/boss/generator/LombokPlugin.java
@@ -0,0 +1,185 @@
+package com.youlaiyouqu.boss.generator;
+
+import org.mybatis.generator.api.IntrospectedColumn;
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.PluginAdapter;
+import org.mybatis.generator.api.dom.java.FullyQualifiedJavaType;
+import org.mybatis.generator.api.dom.java.Interface;
+import org.mybatis.generator.api.dom.java.Method;
+import org.mybatis.generator.api.dom.java.TopLevelClass;
+
+import java.util.List;
+
+/**
+ * @author Liweizhou  2018/6/6
+ */
+public class LombokPlugin extends PluginAdapter {
+
+    private FullyQualifiedJavaType dataAnnotation;
+
+    public LombokPlugin() {
+        dataAnnotation = new FullyQualifiedJavaType("org.springframework.stereotype.Repository");
+    }
+
+    @Override
+    public boolean validate(List<String> warnings) {
+        return true;
+    }
+
+    /**
+     * 拦截 普通字段
+     * @param topLevelClass
+     * @param introspectedTable
+     * @return
+     */
+    @Override
+    public boolean modelBaseRecordClassGenerated(TopLevelClass topLevelClass,
+                                                 IntrospectedTable introspectedTable) {
+        addDataAnnotation(topLevelClass);
+        return true;
+    }
+
+    /**
+     * 拦截 主键
+     * @param topLevelClass
+     * @param introspectedTable
+     * @return
+     */
+    @Override
+    public boolean modelPrimaryKeyClassGenerated(TopLevelClass topLevelClass,
+                                                 IntrospectedTable introspectedTable) {
+        addDataAnnotation(topLevelClass);
+        return true;
+    }
+
+    /**
+     * 拦截 blob 类型字段
+     * @param topLevelClass
+     * @param introspectedTable
+     * @return
+     */
+    @Override
+    public boolean modelRecordWithBLOBsClassGenerated(
+            TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
+        addDataAnnotation(topLevelClass);
+        return true;
+    }
+
+    /**
+     * 防止所有getter被生成
+     * See SimpleModelGenerator
+     * @param method
+     * @param topLevelClass
+     * @param introspectedColumn
+     * @param introspectedTable
+     * @param modelClassType
+     * @return
+     */
+    @Override
+    public boolean modelGetterMethodGenerated(Method method,
+                                              TopLevelClass topLevelClass,
+                                              IntrospectedColumn introspectedColumn,
+                                              IntrospectedTable introspectedTable,
+                                              ModelClassType modelClassType) {
+        return false;
+    }
+
+    /**
+     * 防止所有setter被生成
+     * See SimpleModelGenerator
+     * @param method
+     * @param topLevelClass
+     * @param introspectedColumn
+     * @param introspectedTable
+     * @param modelClassType
+     * @return
+     */
+    @Override
+    public boolean modelSetterMethodGenerated(Method method,
+                                              TopLevelClass topLevelClass,
+                                              IntrospectedColumn introspectedColumn,
+                                              IntrospectedTable introspectedTable,
+                                              ModelClassType modelClassType) {
+        return false;
+    }
+
+    /**
+     * 添加@数据lombok导入和注释的类
+     * @param topLevelClass
+     */
+    protected void addDataAnnotation(TopLevelClass topLevelClass) {
+        //添加domain的import
+//        topLevelClass.addImportedType(dataAnnotation);
+        topLevelClass.addImportedType("lombok.Data");
+        topLevelClass.addImportedType("lombok.Builder");
+        topLevelClass.addImportedType("lombok.NoArgsConstructor");
+        topLevelClass.addImportedType("lombok.AllArgsConstructor");
+
+        //添加domain的注解
+        topLevelClass.addAnnotation("@Data");
+        topLevelClass.addAnnotation("@Builder");
+        topLevelClass.addAnnotation("@NoArgsConstructor");
+        topLevelClass.addAnnotation("@AllArgsConstructor");
+
+    }
+
+//    @Override
+//    public boolean validate(List<String> list) {
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean modelBaseRecordClassGenerated(TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
+//        //添加domain的import
+//        topLevelClass.addImportedType("lombok.Data");
+//        topLevelClass.addImportedType("lombok.Builder");
+//        topLevelClass.addImportedType("lombok.NoArgsConstructor");
+//        topLevelClass.addImportedType("lombok.AllArgsConstructor");
+//
+//        //添加domain的注解
+//        topLevelClass.addAnnotation("@Data");
+//        topLevelClass.addAnnotation("@Builder");
+//        topLevelClass.addAnnotation("@NoArgsConstructor");
+//        topLevelClass.addAnnotation("@AllArgsConstructor");
+//
+//        //添加domain的注释
+//        topLevelClass.addJavaDocLine("/**");
+//        topLevelClass.addJavaDocLine("* Created by Mybatis Generator on " + date2Str(new Date()));
+//        topLevelClass.addJavaDocLine("*/");
+//
+//        return true;
+//    }
+//
+
+    /**
+     * 添加@数据mapper导入和注释的类
+     * @param interfaze
+     * @param topLevelClass
+     * @param introspectedTable
+     * @return
+     */
+    @Override
+    public boolean clientGenerated(Interface interfaze, TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
+        //Mapper文件的注释
+        interfaze.addAnnotation("@Repository");
+        interfaze.addImportedType(dataAnnotation);
+        return true;
+    }
+//
+//    @Override
+//    public boolean modelSetterMethodGenerated(Method method, TopLevelClass topLevelClass, IntrospectedColumn introspectedColumn, IntrospectedTable introspectedTable, ModelClassType modelClassType) {
+//        //不生成getter
+//        return false;
+//    }
+//
+//    @Override
+//    public boolean modelGetterMethodGenerated(Method method, TopLevelClass topLevelClass, IntrospectedColumn introspectedColumn, IntrospectedTable introspectedTable, ModelClassType modelClassType) {
+//        //不生成setter
+//        return false;
+//    }
+//
+//    private String date2Str(Date date) {
+//        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
+//        return sdf.format(date);
+//    }
+}
---
+++ src/main/java/com/youlaiyouqu/boss/generator/RunClass.java
@@ -0,0 +1,101 @@
+package com.youlaiyouqu.boss.generator;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * mybatis逆向工程,会把原来的代码覆盖，慎用
+ */
+
+public class RunClass {
+
+    public int a = 0;
+
+//    private static int x = 100;
+
+    public static void main(String[] args) throws Exception{
+//        RunClass hs1 = new RunClass();
+//            hs1.x++;
+//        RunClass hs2 = new RunClass();
+//            hs2.x++;
+//            hs1 = new RunClass();
+//            hs1.x++;
+//        RunClass.x--;
+//            System.out.println("x=" + x);102
+
+
+//        RunClass app = new RunClass();
+//        System.out.println(app.getClass().getResource("/").getPath());
+//        app.generator();
+//        System.out.println(System.getProperty("user.dir"));
+    }
+
+//    public void method(){
+//        try {
+//            throw new Exception();
+//        } catch (Exception e) {
+//            a=1;
+//            return;
+//        } finally {
+//            a=2;
+//        }
+//        a=3;编译报错
+//    }
+
+    public void generator() throws Exception{
+        List<String> warnings = new ArrayList<>();
+        boolean overwrite = true;
+        InputStream resourceAsStream = this.getClass().getClassLoader().getResourceAsStream("mybatis-generator.xml");
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(resourceAsStream);
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+        myBatisGenerator.generate(null);
+        for(String warning:warnings){
+            System.out.println(warning);
+        }
+    }
+
+//        StringBuilder sb = new StringBuilder();
+//        String id = "B044C53B38BA4E84B507E62402683E26";
+//        String sourceId = "50D530BBA2394D25A0476182D8834ACD";
+//
+//        try {
+//            CloseableHttpClient client = null;
+//            CloseableHttpResponse response = null;
+//            try {
+//                AppUser appUser = loginService.getAppUserMsg("","",sourceId);
+//                HttpGet httpGet = new HttpGet(Variables.sendRefundUrl + "?id="+id+"&sourceId="+sourceId);
+//                httpGet.setHeader("token",loginService.getToken(appUser));
+//
+//                client = HttpClients.createDefault();
+//                response = client.execute(httpGet);
+//                HttpEntity entity = response.getEntity();
+//                String result = EntityUtils.toString(entity);
+//                System.out.println(result);
+//            } finally {
+//                if (response != null) {
+//                    response.close();
+//                }
+//                if (client != null) {
+//                    client.close();
+//                }
+//            }
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        sb.append(id).append("&").append(sourceId);
+//        GouldUtils.doPost(Variables.sendRefundUrl,sb.toString(), ContentType.APPLICATION_JSON);
+
+//        Map<String, String> params = Maps.newHashMap();
+//        params.put("id", id);
+//        params.put("sourceId", sourceId);
+//        GouldUtils.doPost(Variables.sendRefundUrl, params);
+//        HttpUtils.doPost(Variables.sendRefundUrl,sb.toString());
+}
